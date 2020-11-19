97db925648d7c37f625491894133087037bd275e
pausin10 <pausin10@icloud.com>
2020-11-19 17:04:51 +0100

    update hbs

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = require("router");
+const exphbs = require("express-handlebars");
+const path = require('path');
+const passport = require('passport');
+const morgan = require('morgan');
+
+require ('./passport/local-auth');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'index',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('', require('./routes/routes'));
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on ${app.get('port')}`);
+});
