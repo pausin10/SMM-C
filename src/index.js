fa277e1e16589614880ea63d7088d45247340a5f
pausin10 <pausin10@icloud.com>
2020-12-05 14:01:41 +0100

    feat(VID-28):BackEnd for Rooms private/public

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const passport = require('passport');
 const morgan = require('morgan');
 const flash = require('connect-flash');
 const cors = require('cors');
-const SocketIO = require('socket.io');
+
 const ss = require('socket.io-stream');
 const fs = require('fs');
 const rtsp = require('rtsp-ffmpeg');
 const postit = require('./models/postit.js');
 const User = require('./models/user');
-const Rooms = require('./models/room');
+const room = require('./models/room');
 
 const app = express();
 require('./database');
@@ -21,21 +21,25 @@ require('./passport/local-auth');
 
 app.set('port', process.env.PORT || 3000);
 
+const ifEqu = (arg1, arg2, options) => {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+}
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     defaultLayout: 'index',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs'
+    extname: '.hbs',
+    helpers: { ifEqu: ifEqu }
 }));
 app.set('view engine', '.hbs');
 
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'sockets')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'sockets')));
 app.use(session({
     secret: 'key',
     resave: false,
@@ -58,40 +62,6 @@ app.use('', require('./routes/routes'));
 const server = app.listen(app.get('port'), () => {
     console.log(`Server on ${app.get('port')}`);
 });
+const io = require('socket.io')(server);
+require('./sockets/socketBE')(io);
 
-const io = SocketIO(server);
-
-io.on('connection', async (socket) => {
-    console.log('Connected', socket.id);
-    socket.on('joinRoom', (socketRoom) => {
-        socket.join(socketRoom);
-        io.sockets.in(socketRoom).emit('room:message', 'what is going on, party people?');
-        io.sockets.in(socketRoom).emit() 
-
-        socket.on('postit:message', (data) => {
-            io.sockets.in(socketRoom).emit('postit:message', data);
-            addPostit(data);
-        });
-    })
-});
-
-
-
-io.on('error', () => {
-    console.log('Error');
-});
-
-async function addPostit(data) {
-    const newPostit = new postit({
-        user: data.username,
-        description: data.message
-    });
-    await newPostit.save((err) => {
-        if (err) {
-            console.log('Error al guardar el postit en la BD. ' + err);
-            req.flash('error', 'Error al guardar');
-        } else {
-            console.log('Exito al guardar el postit en la BD');
-        }
-    });
-}
--- src/models/group.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
-const groupSchema = new Schema ({
-    name: {type:String, default: _id},
-    members: {type:String}
-});
-
-module.exports = mongoose.model('group', groupSchema);
