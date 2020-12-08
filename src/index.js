7b1537a4b8b8c8770d384c7f835cabe8e5e0c184
sara <saramojon@gmail.com>
2020-12-08 16:37:44 +0100

    feat(VID-6): hbs login/signin

--- src/index.js
+++ src/index.js
@@ -40,6 +40,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'sockets')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: 'key',
     resave: false,
