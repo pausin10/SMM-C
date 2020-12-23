be1fb9c8d7f9c66694f44672ec3e100c28816537
pausin10 <pausin10@icloud.com>
2020-12-23 18:42:34 +0100

    feat(VID-36):Send Emails to users when create room

--- src/email.js
+++ src/email.js
@@ -28,3 +28,50 @@ exports.sendMailSingUp = (mailSignup) => {
     });
 };
 
+
+exports.sendMailAddRoom = (mail, nombreSala, typeroom, password) => {
+    const accountValue = process.env.ACCOUNT;
+    const passValue = process.env.PASS;
+    let mailOptions = null;
+
+    let transport = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: accountValue,
+            pass: passValue
+        }
+    });
+
+    if (typeroom == 'public') {
+        mailOptions = {
+            from: "Streaming Learning",
+            to: mail,
+            subject: 'Streaming Learning',
+            html: 'Your teammate ' + mail + ' add you to a new room.<br>'
+                + 'The name of the room is <Strong>' + nombreSala + '</Strong> and is <Strong>' + typeroom + '</Strong><br><br>'
+                + 'Team Streaming Learning @2021'
+
+        };
+    }
+    if (typeroom == 'private') {
+        mailOptions = {
+            from: "Streaming Learning",
+            to: mail,
+            subject: 'Streaming Learning',
+            html: 'Your teammate ' + mail + ' add you to a new room.<br>'
+                + 'The name of the room is <Strong>' + nombreSala + '</Strong> and is <Strong>' + typeroom + '</Strong><br>'
+                + 'The password for this room is <Strong>' + password + '</Strong><br><br>'
+                + 'Team Streaming Learning @2021'
+
+        };
+    }
+
+    transport.sendMail(mailOptions, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Email sent to: " + mail);
+        }
+    });
+
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,21 +4,22 @@ const user = require('../models/user');
 const postit = require('../models/postit');
 const room = require('../models/room');
 const video = require('../models/video');
+const email = require('../email');
 
 router.get('/home', isAuthenticated, async (req, res) => {
     const rooms = await room.find().lean();
-    const myVideos= await video.find().lean();
+    const myVideos = await video.find().lean();
     res.render('general/home', {
         myUser: req.user.email,
         myVideos,
         rooms: { ...rooms }
-        
+
     });
 });
 
 router.get('/postit', isAuthenticated, async (req, res) => {
     const myPostits = await postit.find({ user: req.user.email }).lean();
-    res.render('general/postits', { 
+    res.render('general/postits', {
         myPostits,
         myUser: req.user.email
     });
@@ -39,6 +40,22 @@ router.get('/addRoom', isAuthenticated, async (req, res) => {
 });
 
 router.post('/addRoom', isAuthenticated, async (req, res) => {
+
+    const usersToSendMail = [...new Set(req.body.sendMail.split(","))];
+    const usersDB = await user.find().lean();
+    if (usersToSendMail) {
+        for (let i = 0; i < usersDB.length; i++) {
+            for (let j = 0; j < usersToSendMail.length; j++) {
+                const mailAddRoom = usersToSendMail[j];
+                const mailDB = usersDB[i].email;
+                if (mailDB == mailAddRoom) {
+                    email.sendMailAddRoom(mailDB, req.body.nombreSala, req.body.typeRoom, req.body.password);
+                }
+            }
+
+        }
+    }
+
     const newRoom = new room({
         name: req.body.nombreSala,
         type: req.body.typeRoom,
@@ -59,8 +76,8 @@ router.post('/addRoom', isAuthenticated, async (req, res) => {
 
 router.get('/addRoom/delete/:id', isAuthenticated, async (req, res) => {
 
-    await room.countDocuments({ _id: req.params.id,createdBy:req.user.email},async (err,count)=>{
-        if(count==1) {
+    await room.countDocuments({ _id: req.params.id, createdBy: req.user.email }, async (err, count) => {
+        if (count == 1) {
             await room.findByIdAndDelete({ _id: req.params.id });
         }
         else console('Imposible eliminar, no eres el creador de la sala');
